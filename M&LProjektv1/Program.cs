c4cffc9d2f6bc01a59a58399b5a687a84274afec
Ludvig24 <lute5@student.eadania.dk>
2025-09-15 09:38:54 +0200

    Add project files.

---
+++ M&LProjektv1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M_LProjektv1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Introduktion
+
+
+
+
+
+        }
+        { //LUDVIGS SPIL HNG
+
+        }
+
+
+
+
+        {//MADS SPIL 
+        
+    
+        }
+
+        
+    }
+}
